29e6690057cd800da7650cee90b424ff14be4edf
Aishe <aishe3819@gmail.com>
2020-05-27 13:24:40 +0300

    commit

--- src/components/AppNavigation.vue
+++ src/components/AppNavigation.vue
@@ -1,11 +1,17 @@
 <template>
     <span>
-        <v-navigation-drawer app v-model="drawer" class="brown lighten-2" dark disable-resize-watcher>
+        <v-navigation-drawer
+            app
+            v-model="drawer"
+            class="brown lighten-2"
+            dark
+            disable-resize-watcher
+        >
             <v-list>
                 <template v-for="(item, index) in items">
                     <v-list-tile :key="index">
                         <v-list-tile-content>
-                            {{item.title}}
+                            {{ item.title }}
                         </v-list-tile-content>
                     </v-list-tile>
                     <v-divider :key="`divider-${index}`"></v-divider>
@@ -13,15 +19,32 @@
             </v-list>
         </v-navigation-drawer>
         <v-toolbar app color="brown darken-4" dark>
-            <v-toolbar-side-icon class="hidden-md-and-up" @click="drawer = !drawer"></v-toolbar-side-icon>
+            <v-toolbar-side-icon
+                class="hidden-md-and-up"
+                @click="drawer = !drawer"
+            ></v-toolbar-side-icon>
             <v-spacer class="hidden-md-and-up"></v-spacer>
             <router-link to="/">
-                <v-toolbar-title to="/">{{appTitle}}</v-toolbar-title>
+                <v-toolbar-title to="/">{{ appTitle }}</v-toolbar-title>
             </router-link>
-            <v-btn flat class="hidden-sm-and-down" to="/menu">Menu</v-btn>
+            <v-btn
+                depressed
+                color="brown darken-4"
+                class="hidden-sm-and-down"
+                to="/menu"
+                >Menu</v-btn
+            >
             <v-spacer class="hidden-sm-and-down"></v-spacer>
-            <v-btn flat class="hidden-sm-and-down" to="/sign-in">SIGN IN</v-btn>
-            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join">JOIN</v-btn>
+            <v-btn
+                depressed
+                color="brown darken-4"
+                class="hidden-sm-and-down"
+                to="/sign-in"
+                >SIGN IN</v-btn
+            >
+            <v-btn color="brown lighten-3" class="hidden-sm-and-down" to="/join"
+                >JOIN</v-btn
+            >
         </v-toolbar>
     </span>
 </template>
@@ -34,7 +57,7 @@ export default {
             drawer: false,
             items: [
                 { title: 'Menu' },
-                { title: 'Profile' }, 
+                { title: 'Profile' },
                 { title: 'Sign In' },
                 { title: 'Join' }
             ]
@@ -44,7 +67,7 @@ export default {
 </script>
 
 <style scoped>
-a {
+.v-application a {
     color: white;
     text-decoration: none;
 }
